37d2d804255d7c19e5602f81d3e9407138965ab3
league <league@imac-18.attlocal.net>
2022-02-28 17:01:10 -0800

    new token

--- src/_07_The_Wrong_Way_Cow/TheWrongWayCow.java
+++ src/_07_The_Wrong_Way_Cow/TheWrongWayCow.java
@@ -48,21 +48,93 @@
 
 package _07_The_Wrong_Way_Cow;
 
+import java.util.Arrays;
+
 public class TheWrongWayCow {
 
-    public static int[] findWrongWayCow(final char[][] field) {
-        // Fill in the code to return the [col, row] coordinate position of the
-        // head (letter 'c') of the wrong way cow!
-    	String letter = "c";
-    	Char c
-        for (int i = 0; i < field.length; i++) {
+	public static int[] findWrongWayCow(final char[][] field) {
+		// Fill in the code to return the [col, row] coordinate position of the
+		// head (letter 'c') of the wrong way cow!
+		char c = 'c';
+		char o = 'o';
+		char w = 'w';
+
+		int north = 0;
+		int west = 0;
+		int south = 0;
+		int east = 0;
+
+		int[] lastNorth = new int[2];
+		int[] lastSouth = new int[2];
+		int[] lastWest = new int[2];
+		int[] lastEast = new int[2];
+
+		int[] Finale = new int[2];
+
+		for (int i = 0; i < field.length; i++) {
 			for (int j = 0; j < field.length; j++) {
-			
-				if (field[i][j] == "c") {
+
+				if (field[i][j] == c) {
+
 					
+					if (i <= field.length - 1 - 2) {
+						if (field[i + 1][j] == o) {
+							if (field[i + 2][j] == w) {
+								west++;
+								lastWest = new int[] { j, i };
+							}
+						}
+					}
+					
+				
+						if (i >= 2) {
+						if (field[i - 1][j] == o) {
+
+							if (field[i - 2][j] == w) {
+								east++;
+								lastEast = new int[] { j, i };
+							}
+						}
+						}
+					
+					if (j <= field[i].length - 1 - 2) {
+						if (field[i][j + 1] == o) {
+
+							if (field[i][j + 2] == w) {
+								north++;
+								lastNorth = new int[] { i, j };
+							}
+
+						}
+					}
+					if (j >= 2) {
+						if (field[i][j - 1] == o) {
+
+							if (field[i][j - 2] == w) {
+								south++;
+								lastSouth = new int[] { i, j };
+							}
+
+						}
+
+					}
 				}
 			}
 		}
-        return null;
-    }
+		System.out.println(north);
+		if (west == 1) {
+			Finale = lastWest;
+		}
+		if (east == 1) {
+			Finale = lastEast;
+		}
+		if (north == 1) {
+			Finale = lastNorth;
+		}
+		if (south == 1) {
+			Finale = lastSouth;
+		}
+		System.out.println(Arrays.toString(Finale));
+		return Finale;
+	}
 }
